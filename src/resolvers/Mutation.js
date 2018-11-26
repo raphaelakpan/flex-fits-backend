da4d9711d301dbe80d038080515a7dabacc54842
Raphael Akpan <raphael.akpan@andela.com>
2018-11-26 22:13:17 +0100

    Create and retrieve items - Define Item data model - Add GraphQL-Yoga mutation and query - Add Prisma resolvers for mutation and query

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutation = {};
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({
+      data: { ...args }
+    }, info);
+
+    return item;
+  }
+};
 
 module.exports = Mutation;
