fac8a17d6fbecb8c3f278c7b6303e0c51ca84b47
Raphael Akpan <raphael.akpan@andela.com>
2018-12-01 21:55:33 +0100

    Add updateItem Mutation and resolver - Add item query and resolver

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,7 +1,15 @@
 const Mutation = {
   async createItem(parent, args, ctx, info) {
     const item = await ctx.db.mutation.createItem({
-      data: { ...args }
+      data: { ...args.data }
+    }, info);
+
+    return item;
+  },
+  async updateItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.updateItem({
+      data: args.data,
+      where: args.where,
     }, info);
 
     return item;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,6 +2,10 @@ const Query = {
   async items(parent, args, ctx, info) {
     const items = await ctx.db.query.items();
     return items;
+  },
+  async item(parent, args, ctx, info) {
+    const item = await ctx.db.query.item({ where: args.where });
+    return item;
   }
 };
 
