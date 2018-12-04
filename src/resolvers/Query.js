ebad00c85b87d2fa92b80fe3cd61287fcb40f716
Raphael Akpan <raphael.akpan@andela.com>
2018-12-04 08:50:39 +0100

    Query and resolver to support Pagination

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,12 +1,12 @@
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-  async items(parent, args, ctx, info) {
-    const items = await ctx.db.query.items();
-    return items;
-  },
+  items: forwardTo('db'),
   async item(parent, args, ctx, info) {
     const item = await ctx.db.query.item({ where: args.where });
     return item;
-  }
+  },
+  itemsConnection: forwardTo('db'),
 };
 
 module.exports = Query;
